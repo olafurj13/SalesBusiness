bac350bd6413391b84e64aa5c6cc94a7d725d788
Sveinn Henrik Kristinsson <Svenni@Sveinns-Air.lan>
2016-03-04 15:18:54 +0000

    Svenni

---
+++ src/components/product/ProductController.js
@@ -0,0 +1,10 @@
+"use strict";
+
+angular.module("project3App").controller("ProductController",
+function SellersController($scope, AppResource, $location) {
+	// TODO: load data from AppResource! Also, add other methods, such as to
+	// add/update sellers etc.
+	var getSellersPromise = AppResource.getSellers();
+	getSellersPromise.success(function(sellers){
+		$scope.sellers = sellers;
+	});
