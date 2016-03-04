957a64b748d76f54278ecde6782eeb7fec812d47
Andri Freysson <andrifreysson@Andris-MacBook-Pro.local>
2016-03-04 10:59:46 +0000

    First PAge

--- src/components/sellers/SellersController.js
+++ src/components/sellers/SellersController.js
@@ -4,5 +4,10 @@ angular.module("project3App").controller("SellersController",
 function SellersController($scope, AppResource) {
 	// TODO: load data from AppResource! Also, add other methods, such as to
 	// add/update sellers etc.
+	var getSellersPromise = AppResource.getSellers();
+	getSellersPromise.success(function(sellers){
+		console.log(sellers);
+		$scope.sellers = sellers;
+	});
 
 });
