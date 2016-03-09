8842f7d4f918fcfcce1233bd8c410602e8dcb9e4
olafurj13 <olafur9@gmail.com>
2016-03-09 23:29:39 +0000

    Vinna í testum, fyrsta komið í gegn - OHJ

--- src/components/addseller/AddNewSellerController.spec.js
+++ src/components/addseller/AddNewSellerController.spec.js
@@ -1,12 +1,38 @@
 "use strict";
 
-describe("AddNewSellerController", function() {
+describe("AddNewSellerController", function(AppResource) {
 	beforeEach(module("project3App"));
-	
-	var mockSeller = {
-		name : "name",
-		category: "category",
-		imagePath: "imagepath"
+	var scope, AddNewSellerController;
+	var mockCentrisNotify = {
+			success: function success(message) {
+		},
+			error: function error(message) {
+		}
 	};
 
+	var mockLocation = {
+		path: function(p) {
+		}
+	};
+
+	beforeEach(inject(function ($rootScope, $controller) {
+		scope = $rootScope.$new();
+		AddNewSellerController = $controller('AddNewSellerController', {
+			$scope: scope,
+			$location: mockLocation,
+			AppResource: AppResource,
+			centrisNotify: mockCentrisNotify
+		});
+	}));
+	
+	it("add seller should be correctly defined", function(){
+		expect(scope.addSeller).toBeDefined();
+	});
+
+	// it("add seller should be added", function(){
+	// 	spyOn(mockCentrisNotify, "success");
+	// 	spyOn(mockCentrisNotify, "error");
+	// 	expect(mockCentrisNotify.success).toHaveBeenCalledWith("str1");
+	// 	expect(mockCentrisNotify.error).not.toHaveBeenCalled();
+	// });
 });
