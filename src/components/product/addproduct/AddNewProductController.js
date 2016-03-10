5f91cf24830247f674d64dac578bc8aac99d51d3
Sveinn Henrik Kristinsson <Svenni@Sveinns-Air.lan>
2016-03-10 00:28:24 +0000

    Product validation

--- src/components/product/addproduct/AddNewProductController.js
+++ src/components/product/addproduct/AddNewProductController.js
@@ -1,7 +1,7 @@
 "use strict";
 
 angular.module("project3App").controller("AddNewProductController",
-function AddNewProductController($scope, $location, $routeParams,AppResource) {
+function AddNewProductController($scope, $location, $routeParams,AppResource, centrisNotify) {
 	
 	var product_obj;
 	console.log($routeParams.sellerid);
@@ -16,10 +16,22 @@ function AddNewProductController($scope, $location, $routeParams,AppResource) {
 			quantityInStock: $scope.quantityInStock,
 			imagePath: $scope.imagePath
 		};
-
-		AppResource.addSellerProduct(sellerid, product_obj).success(function(product){
-			console.log("success");
-		});
+		if(product_obj.name === ""){
+			centrisNotify.error("product.Messages.MissingAName", "æjæj");
+		} else if(product_obj.price === ""){
+			centrisNotify.error("product.Messages.MissingAPrice");
+		}else if(product_obj.quantitySold === ""){
+			centrisNotify.error("product.Messages.MissingAQuantatySold");
+		}else if(product_obj.quantityInStock === ""){
+			centrisNotify.error("product.Messages.MissingAQuantityInStock");
+		} else if(product_obj.imagePath === ""){
+			centrisNotify.error("product.Messages.MissingAImage");
+		} else {
+			AppResource.addSellerProduct(sellerid, product_obj).success(function(product){
+				centrisNotify.success("Success","success");
+				console.log("success");
+			});
+		}
 	};
 	$scope.back = function back(){
 		$location.path("/seller/" + $routeParams.sellerid);
--- src/components/product/editproduct/EditProductController.js
+++ src/components/product/editproduct/EditProductController.js
@@ -1,7 +1,7 @@
 "use strict";
 
 angular.module("project3App").controller("EditProductController",
-function EditProductController($scope, $location, $routeParams, AppResource) {
+function EditProductController($scope, $location, $routeParams, AppResource, centrisNotify) {
 	// TODO: load data from AppResource! Also, add other methods, such as to
 	// add/update sellers etc.
 	console.log($routeParams.id);
@@ -22,9 +22,23 @@ function EditProductController($scope, $location, $routeParams, AppResource) {
 	
 	$scope.editProduct = function editProduct(){
 		console.log($scope.products);
-		AppResource.updateProduct($scope.products.id, $scope.products).success(function(p){
+		if($scope.products.name === ""){
+			centrisNotify.error("product.Messages.MissingEName", "æjæj");
+		} else if($scope.products.price === ""){
+			centrisNotify.error("product.Messages.MissingEPrice");
+		}else if($scope.products.quantitySold === ""){
+			centrisNotify.error("product.Messages.MissingEQuantatySold");
+		}else if($scope.products.quantityInStock === ""){
+			centrisNotify.error("product.Messages.MissingEQuantityInStock");
+		} else if($scope.products.imagePath === ""){
+			centrisNotify.error("product.Messages.MissingEImage");
+		} else {
+			AppResource.updateProduct($scope.products.id, $scope.products).success(function(p){
 			console.log("success");
-		});
+			centrisNotify.success("Success","success");
+			$location.path("/seller/" + $routeParams.sellerid);
+			});
+		}
 	};
 	$scope.back = function back(){
 		$location.path("/seller/" + $routeParams.sellerid);
